17ae5d02a40074904797d5b6a8b7e47f3785bd9d
Francesco Emanuele D'Agostino <fedagostino@gmail.com>
2026-05-25 14:30:37 +0200

    add OVERRIDE_FIND_PACKAGE in pacage declaration

--- cmake/c-ares.cmake
+++ cmake/c-ares.cmake
@@ -3,6 +3,7 @@ FetchContent_Declare(
   c-ares
   GIT_REPOSITORY https://github.com/c-ares/c-ares.git
   GIT_TAG        v1.34.6
+  OVERRIDE_FIND_PACKAGE
 )
 
 set(CARES_STATIC            ON CACHE BOOL "" FORCE)
@@ -13,4 +14,8 @@ set(c-ares_INCLUDE_DIRS     "${FETCHCONTENT_BASE_DIR}/c-ares-build/"
                             "${FETCHCONTENT_BASE_DIR}/c-ares-src/include/"   )
 set(c-ares_LIBRARIES        "${FETCHCONTENT_BASE_DIR}/c-ares-build/src/lib/" )
 
+mark_as_advanced(c-ares_INCLUDE_DIRS c-ares_LIBRARIES)
+
 FetchContent_MakeAvailable(c-ares)
+
+add_library(c-ares::c-ares ALIAS c-ares)
