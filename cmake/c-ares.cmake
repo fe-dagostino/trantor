9815488b8ae2a0f5de1d8ee91bea7a23eb4c58af
an-tao <antao2002@gmail.com>
2026-05-28 10:37:09 +0800

    Lint

--- cmake/c-ares.cmake
+++ cmake/c-ares.cmake
@@ -1,18 +1,24 @@
-
 FetchContent_Declare(
   c-ares
   GIT_REPOSITORY https://github.com/c-ares/c-ares.git
-  GIT_TAG        v1.34.6
-  OVERRIDE_FIND_PACKAGE
+  GIT_TAG v1.34.6 OVERRIDE_FIND_PACKAGE
 )
 
-set(CARES_STATIC            ON CACHE BOOL "" FORCE)
-set(CARES_STATIC_PIC        ON CACHE BOOL "" FORCE)
-set(CARES_SHARED           OFF CACHE BOOL "" FORCE)
+set(CARES_STATIC
+    ON
+    CACHE BOOL "" FORCE
+)
+set(CARES_STATIC_PIC
+    ON
+    CACHE BOOL "" FORCE
+)
+set(CARES_SHARED
+    OFF
+    CACHE BOOL "" FORCE
+)
 
-set(c-ares_INCLUDE_DIRS     "${FETCHCONTENT_BASE_DIR}/c-ares-build/"
-                            "${FETCHCONTENT_BASE_DIR}/c-ares-src/include/"   )
-set(c-ares_LIBRARIES        "${FETCHCONTENT_BASE_DIR}/c-ares-build/src/lib/" )
+set(c-ares_INCLUDE_DIRS "${FETCHCONTENT_BASE_DIR}/c-ares-build/" "${FETCHCONTENT_BASE_DIR}/c-ares-src/include/")
+set(c-ares_LIBRARIES "${FETCHCONTENT_BASE_DIR}/c-ares-build/src/lib/")
 
 mark_as_advanced(c-ares_INCLUDE_DIRS c-ares_LIBRARIES)
 
